cbecec8c8463be2dba2ebd9ebf148ba5051383b2
wuarty <leonbrasilino@outlook.pt>
2025-02-05 14:37:18 -0300

    correção de bug

--- main.py
+++
@@ -1,7 +0,0 @@
-nome = input('Digite seu nome:')
-altura = float(input('Digite sua altura: '))
-peso = float(input('Digite seu peso; '))
-imc = peso / (altura ** 2)
-
-
-print(f'O imc de {nome} é de', imc)
