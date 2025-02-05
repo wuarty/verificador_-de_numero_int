645e68faf3e4e0d35506e1b8b0bbd137a102274f
Leon <leonbrasilino@outlook.pt>
2025-02-05 12:35:17 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+nome = input('Digite seu nome:')
+altura = float(input('Digite sua altura: '))
+peso = float(input('Digite seu peso'))
+imc = peso / (altura ** 2)
+
+
+print('O imc de', nome, 'é de', imc)
