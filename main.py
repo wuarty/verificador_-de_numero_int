c3a331763133bde7163f91ded430c7e440f0ab23
Leon <leonbrasilino@outlook.pt>
2025-02-05 15:06:24 -0300

    correção de bugs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 nome = input('Digite seu nome:')
 altura = float(input('Digite sua altura: '))
-peso = float(input('Digite seu peso'))
+peso = float(input('Digite seu peso; '))
 imc = peso / (altura ** 2)
 
 
